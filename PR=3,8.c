6f29ffc605cadef484172efc67f3608b21b9c9a6
smitbuha25 <130464705+smitbuha25@users.noreply.github.com>
2023-04-26 17:32:46 +0530

    Add files via upload

---
+++ PR=3,8.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+main()
+{
+    int n,i,num=0;
+	
+	printf("Enter Number :");
+	scanf("%d",&n);
+	
+	if (n==0 && n==1)
+	{
+		printf("%d is not a prime number.",n);
+    }
+	else
+    {
+	   	for (i=2;i<=n/2;i++)
+	   	
+	    {
+		if(n%i==0)
+		  {
+		    	num=1;
+	      }
+	    }
+	    if (num == 0)
+	 	{
+		printf("%d is a prime number.",n);
+	    }
+	    else
+	    {
+	    	printf("%d  is not prine number.",n);
+		}
+	}
+	return 0;	
+}
